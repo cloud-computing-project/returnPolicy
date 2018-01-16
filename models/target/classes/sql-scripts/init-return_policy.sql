833b2bee8e2efad55b853a1df4444e746840e239
Amela Špica <amela.spica@gmail.com>
2018-01-16 20:44:35 +0100

    after build

---
+++ models/target/classes/sql-scripts/init-return_policy.sql
@@ -0,0 +1,3 @@
+INSERT INTO return_policy (id, contact_seller_within, refund, restocking_fee, product_id) VALUES (1, '2', true, '2,3', 1);
+INSERT INTO return_policy (id, contact_seller_within, refund, restocking_fee, product_id) VALUES (2, '2', false, '4,3', 1);
+INSERT INTO return_policy (id, contact_seller_within, refund, restocking_fee, product_id) VALUES (3, '2', true, '49,2', 2);
