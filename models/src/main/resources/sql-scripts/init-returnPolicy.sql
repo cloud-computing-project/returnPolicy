b9c3c4754f75a96117493dc9ee074360c4b692df
Amela Špica <amela.spica@gmail.com>
2018-01-18 20:54:46 +0100

    etcd sql

--- models/src/main/resources/sql-scripts/init-returnPolicy.sql
+++ models/src/main/resources/sql-scripts/init-returnPolicy.sql
@@ -1,3 +1,3 @@
-INSERT INTO returnPolicy (id, contact_seller_within, refund, restocking_fee, product_id) VALUES (1, '2 days', 'yes', '4,4', 1);
-INSERT INTO returnPolicy (id, contact_seller_within, refund, restocking_fee, product_id) VALUES (2, '3 days', 'no','3,3', 2);
-INSERT INTO returnPolicy (id, contact_seller_within, refund, restocking_fee, product_id) VALUES (3, '1 day', 'no', '2,2', 1);
+INSERT INTO returnPolicy (id, contact_seller_within, refund, restocking_fee) VALUES (1, '2 days', 'yes', '4,4');
+INSERT INTO returnPolicy (id, contact_seller_within, refund, restocking_fee) VALUES (2, '3 days', 'no','3,3');
+INSERT INTO returnPolicy (id, contact_seller_within, refund, restocking_fee) VALUES (3, '1 day', 'no', '2,2');
